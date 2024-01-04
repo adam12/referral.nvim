9571c05cffcf4a49801591e58982ee6df0c73abb
Adam Daniels <adam@mediadrive.ca>
2024-01-04 14:40:18 -0500

    Initial commit

---
+++ lua/referral/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+local Job = require('plenary.job')
+
+M.setup = function(config)
+  config = config or {}
+
+  -- Set up binding for :Referral <x>
+  vim.api.nvim_create_user_command('Referral', function(opts)
+    print('Running ' .. string.format([['referral %s']], opts.args))
+    vim.fn.setqflist({}, 'r')
+
+    Job:new({
+      command = 'referral',
+      args = vim.split(opts.args, ' '),
+      on_exit = function(j, return_val)
+        local data = j:result()
+
+        if return_val == 0 then
+          vim.schedule(function()
+            vim.fn.setqflist({}, 'r', {title=string.format('Referral %s', opts.args), lines=data})
+          end)
+        end
+
+        print('Referral done!')
+      end,
+      on_stderr = function(_, msg)
+        print(msg)
+      end,
+    }):start()
+  end,
+  {
+    desc = 'Look for references in Ruby source code using the Referral tool',
+    nargs = '+',
+    force = true
+  })
+end
+
+return M
