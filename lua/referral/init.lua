b05ee8bc37065a565b937f0b77d749c3793d0ea1
Adam Daniels <adam@mediadrive.ca>
2024-01-15 20:54:38 -0500

    Move user command setup to plugin file

--- lua/referral/init.lua
+++ lua/referral/init.lua
@@ -2,39 +2,33 @@ local M = {}
 local Job = require('plenary.job')
 
 M.setup = function(config)
-  config = config or {}
+  -- Do nothing
+end
 
-  -- Set up binding for :Referral <x>
-  vim.api.nvim_create_user_command('Referral', function(opts)
-    vim.notify('Running ' .. string.format([['referral %s']], opts.args))
-    vim.fn.setqflist({}, 'r')
+M.referral = function(opts)
+  vim.notify('Running ' .. string.format([['referral %s']], opts.args))
+  vim.fn.setqflist({}, 'r')
 
-    Job:new({
-      command = 'referral',
-      args = vim.split(opts.args, ' '),
-      on_exit = function(j, return_val)
-        local data = j:result()
+  Job:new({
+    command = 'referral',
+    args = vim.split(opts.args, ' '),
+    on_exit = function(j, return_val)
+      local data = j:result()
 
-        if return_val == 0 then
-          vim.schedule(function()
-            vim.fn.setqflist({}, 'r', {title=string.format('Referral %s', opts.args), lines=data})
-          end)
-        end
-
+      if return_val == 0 then
         vim.schedule(function()
-          vim.notify('Referral done!')
+          vim.fn.setqflist({}, 'r', {title=string.format('Referral %s', opts.args), lines=data})
         end)
-      end,
-      on_stderr = function(_, msg)
-        vim.notify(msg)
-      end,
-    }):start()
-  end,
-  {
-    desc = 'Look for references in Ruby source code using the Referral tool',
-    nargs = '+',
-    force = true
-  })
+      end
+
+      vim.schedule(function()
+        vim.notify('Referral done!')
+      end)
+    end,
+    on_stderr = function(_, msg)
+      vim.notify(msg)
+    end,
+  }):start()
 end
 
 return M
